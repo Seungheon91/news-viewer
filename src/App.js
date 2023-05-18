c9dd26e718afc5a47396f5cd6f59e3330c43b3ed
StarkLSH <seungheon1029@gmail.com>
2023-05-18 18:07:42 +0900

    styled-component 적용 및 newsList, item 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import { useState } from "react";
-import axios from "../node_modules/axios/index";
+import NewsList from "./components/NewsList";
 
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=a093e7772b51453ca867a20dac1e8185')
-      
-      setData(response.data)
-
-    } catch (e) {
-      console.log(e)
-    }
-   
-  }
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}></textarea>}
-    </div>
-  );
+  return <NewsList />
 }
 
 export default App;
